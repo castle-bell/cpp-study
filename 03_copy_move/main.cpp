05243845576a3fbbc413fd06641f0ef70844b8af
castle-bell <dlwprmaksaks@naver.com>
2026-08-10 16:05:43 +0900

    [03_copy_move] 복사 대입 연산자 구현

--- 03_copy_move/main.cpp
+++ 03_copy_move/main.cpp
@@ -5,15 +5,23 @@ class CopyTracer
 {
 public:
 	CopyTracer(const std::string& name)
-		: Name{ name }
+		: Name{name}
 	{
 		std::cout << "This is Original: " << name << '\n';
 	}
 
 	CopyTracer(const CopyTracer& other)
-		: Name{ other.Name + " Copy" }
+		: Name{other.Name}
 	{
-		std::cout << "This is Copy From: " << other.Name << '\n';
+		std::cout << "Copy constructed from: " << other.Name << '\n';
+	}
+
+	CopyTracer& operator=(const CopyTracer& other)
+	{
+		Name = other.Name;
+		std::cout << "Copy assigned from: " << other.Name << '\n';
+
+		return *this;
 	}
 
 	~CopyTracer()
@@ -32,6 +40,19 @@ private:
 
 int main()
 {
-	CopyTracer original{ "Original" };
-	CopyTracer copy = original;
+	CopyTracer original{"Original"};
+	CopyTracer copied{original};
+
+	std::cout << std::boolalpha
+		<< "Copied object is distinct: " << (&copied != &original) << '\n'
+		<< "Copied value matches: " << (copied.GetName() == original.GetName()) << '\n';
+
+	CopyTracer target{"Target"};
+	const CopyTracer* targetAddress = &target;
+	CopyTracer& assignmentResult = (target = original);
+
+	std::cout
+		<< "Target address unchanged: " << (targetAddress == &target) << '\n'
+		<< "operator= returned target: " << (&assignmentResult == &target) << '\n'
+		<< "Assigned value matches: " << (target.GetName() == original.GetName()) << '\n';
 }
