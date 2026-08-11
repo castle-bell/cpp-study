dc660cf73320c8d8e9cc75c8646f5c298029c5e0
castle-bell <dlwprmaksaks@naver.com>
2026-08-11 14:51:48 +0900

    study: unique ptr move semantics

--- 05_smart_pointer/main.cpp
+++ 05_smart_pointer/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <memory>
+#include <cassert>
+#include <utility>
 
 #include "Monster.h"
 
@@ -10,8 +12,20 @@ void TestUniquePointer()
 	std::cout << "Leaving scope\n";
 }
 
+void TestUniquePointerMove()
+{
+	auto first = std::make_unique<Monster>("Slime");
+	auto moved = std::move(first);
+
+	assert(!first);
+	assert(moved);
+
+	moved->PrintName();
+}
+
 int main()
 {
 	TestUniquePointer();
+	TestUniquePointerMove();
 	return 0;
 }
