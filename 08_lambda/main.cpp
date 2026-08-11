fd38c275772d490adb6f6f107f895e66b9bbeea1
castle-bell <dlwprmaksaks@naver.com>
2026-08-11 16:21:26 +0900

    study: lambda reference capture

--- 08_lambda/main.cpp
+++ 08_lambda/main.cpp
@@ -36,9 +36,41 @@ void TestLambda2()
 	}
 }
 
+void TestLambda3()
+{
+	int bonus = 100;
+
+	auto monsterHPs = std::vector<int>{ 100, 80, 0, 120 };
+	auto condition = [bonus](int hp)
+		{
+			return hp < bonus;
+		};
+
+	auto found = std::find_if(monsterHPs.begin(), monsterHPs.end(), condition);
+
+	if (found != monsterHPs.end())
+	{
+		std::cout << *found << '\n';
+	}
+}
+
+void TestReferenceCapture()
+{
+	int bonus = 10;
+
+	auto addBonus = [&bonus]
+	{
+		bonus += 5;
+	};
+
+	addBonus();
+	std::cout << "Bonus: " << bonus << '\n';
+}
+
 int main()
 {
 	TestLambda1();
 	TestLambda2();
+	TestReferenceCapture();
 	return 0;
 }
